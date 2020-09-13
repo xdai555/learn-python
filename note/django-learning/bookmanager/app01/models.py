f72870ca7fd71b13a372707274826f0e04e2b1eb
xdai <3ricWu@oDai.v1p>
2020-09-13 23:58:55 +0800

    a test repo

--- note/django-learning/bookmanager/app01/models.py
+++ note/django-learning/bookmanager/app01/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 # Create your models here.
-class Publisher(models.Model):
+class publisher(models.Model):
     name = models.CharField(max_length=32)
--- note/django-learning/bookmanager/app01/views.py
+++ note/django-learning/bookmanager/app01/views.py
@@ -1,10 +1,8 @@
-from os import name
 from django.shortcuts import render
 from app01 import models
 # Create your views here.
-def publisher_list(request):
-    # 返回一个页面，包含所有的出版社信息
-    all_publisher = [i.name for i in models.Publisher.objects.all()]
-    print(all_publisher)
-    # 获取所有的出版社信息，返回一个页面，页面中包含出版社信息
-    return render(request,'publisher_list.html',{'all_publisher':all_publisher})
+
+
+def publishers(request):
+    all_publishers = models.publisher.objects.all()
+    return render(request, 'publisher_list.html', {'all_publishers': all_publishers})
